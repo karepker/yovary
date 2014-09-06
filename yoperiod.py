74fe34eef455c071166a6c2a703f0c3a47f74b16
Kar Epker <karepker+services@gmail.com>
2014-09-06 13:51:54 -0400

    Changed request library to urllib and added basic bottle functionality.

--- yoperiod.py
+++ yoperiod.py
@@ -1,38 +1,41 @@
-#import a bunch of shit
+#! /usr/bin/env python3
 
 #gcalendar for reference
 #https://github.com/insanum/gcalcli
 
-
-
 #what's a good way to scale this for many users??
-import requests
+import bottle
 import datetime
+import urllib.error
+import urllib.parse
+import urllib.request
 
-preset_api_token= 'f1e28817-3528-621a-70ac-8086fc300205'
-
-
-
-class EmbrYO_User:
-	api_token= preset_api_token
-	username='SARAHPG'
-
-def single_user_yo( user_class ):
-	payload= {'api_token': user_class.api_token, 'username': user_class.username}
-	return requests.post("http://api.justyo.co/yo/", data=payload)
-
-#Array of users with the same time should be used
-
-def pill_reminder( user_class, timeRequest ):
-	if timeRequest in range[datetime.time-5,datetime.time+5]:
-		#Once array is implemented, this should iterate through the array
-		single_user_yo( user_class )
-	
-testBunny=EmbrYO_User()
-single_user_yo( testBunny )
-
-#def ovary_action( api_token, username, dayPrediction):
+class EmbryoUser:
+    api_token = 'f1e28817-3528-621a-70ac-8086fc300205'
+    username = 'DROMENIC'
+    
+def single_user_yo(user_class):
+    payload = {
+            'api_token': user_class.api_token,
+            'username': user_class.username
+            }
+    data = urllib.parse.urlencode(payload)
+    data = data.encode('utf-8')
+    req = urllib.request.Request('http://api.justyo.co/yo/', data)
+    print(urllib.request.urlopen(req))
 
 
+def pill_reminder(user_class, timeRequest):
+    if timeRequest in range[datetime.time-5,datetime.time+5]:
+        #Once array is implemented, this should iterate through the array
+        single_user_yo(user_class)
+    
+@bottle.route('/')
+def sign_up():
+    return 'Hello World!'
 
-	
+bottle.run(host='localhost', port=8080, debug=True)
+
+
+
+    
